ab83c726f3931db46126eed56fb9931690fb7e8e
Terry Marr & Vaughn Dice <student@epicodus.com>
2015-08-06 15:03:46 -0700

    implementing limited coin availability

--- lib/your_change.rb
+++ lib/your_change.rb
@@ -2,22 +2,29 @@ class Fixnum
   define_method(:your_change) do
     remainder = self
 
-    denom = {'q' => {"denom" => "quarter",
-                     "count" => 0,
-                     "interval" => 25},
-             'd' => {"denom" => "dime",
-                     "count" => 0,
-                     "interval" => 10},
-             'n' => {"denom" => "nickel",
-                     "count" => 0,
-                     "interval" => 5},
-             'p' => {"denom" => "penny",
-                     "count" => 0,
-                     "interval" => 1}}
+    denom = {'q' => {"denom"    => "quarter",
+                     "count"    => 0,
+                     "interval" => 25,
+                     "avail"    => 2},
+
+             'd' => {"denom"    => "dime",
+                     "count"    => 0,
+                     "interval" => 10,
+                     "avail"    => 2},
+
+             'n' => {"denom"    => "nickel",
+                     "count"    => 0,
+                     "interval" => 5,
+                     "avail"    => 4},
+
+             'p' => {"denom"    => "penny",
+                     "count"    => 0,
+                     "interval" => 1,
+                     "avail"    => 1000}}
 
     until remainder.==(0)
       denom.each do |key, coin|
-        until remainder.<(coin.fetch('interval'))
+        until remainder.<(coin.fetch('interval')) || coin.fetch('avail').==(0)
           count = coin.fetch('count')
 
           if count.==(1)
@@ -26,10 +33,10 @@ class Fixnum
           end
 
           coin.store('count', count.+(1))
+          coin.store('avail', coin.fetch('avail').-(1))
           remainder = remainder.-(coin.fetch('interval'))
         end
       end
-      remainder = 0
     end
 
     result = []
--- spec/your_change_spec.rb
+++ spec/your_change_spec.rb
@@ -3,21 +3,23 @@ require('your_change')
 
 describe('Fixnum#your_change') do
   it('accepts an amount and returns how many quarters') do
-    expect(75.your_change()).to(eq('3 quarters'))
+    expect(75.your_change()).to(eq('2 quarters, 2 dimes and 1 nickel'))
     expect(50.your_change()).to(eq('2 quarters'))
     expect(25.your_change()).to(eq('1 quarter'))
   end
 
   it('accepts an amount and returns how many dimes') do
-    expect(70.your_change()).to(eq('2 quarters and 2 dimes'))
+    expect(80.your_change()).to(eq('2 quarters, 2 dimes and 2 nickels'))
     expect(60.your_change()).to(eq('2 quarters and 1 dime'))
   end
 
   it('accepts an amount and returns how many nickels') do
     expect(65.your_change()).to(eq('2 quarters, 1 dime and 1 nickel'))
+    expect(90.your_change()).to(eq('2 quarters, 2 dimes and 4 nickels'))
   end
 
   it('accepts and amount and returns how many pennies') do
+    expect(99.your_change()).to(eq('2 quarters, 2 dimes, 4 nickels and 9 pennies'))
     expect(67.your_change()).to(eq('2 quarters, 1 dime, 1 nickel and 2 pennies'))
     expect(1.your_change()).to(eq('1 penny'))
   end
