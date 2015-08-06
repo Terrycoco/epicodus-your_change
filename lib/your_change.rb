b5fffd8cab9561858c05b1ff0a1f49c8d254356f
Terry Marr & Vaughn Dice <student@epicodus.com>
2015-08-06 14:12:13 -0700

    looping version complete, taking any denomination and returning correct amount

--- lib/your_change.rb
+++ lib/your_change.rb
@@ -1,7 +1,48 @@
 class Fixnum
   define_method(:your_change) do
-    if self.<(5)
-      return self.to_s.concat( ' pennies')
+    remainder = self
+
+    denom = {'q' => {"denom" => "quarter",
+                     "count" => 0,
+                     "interval" => 25},
+             'd' => {"denom" => "dime",
+                     "count" => 0,
+                     "interval" => 10},
+             'n' => {"denom" => "nickel",
+                     "count" => 0,
+                     "interval" => 5},
+             'p' => {"denom" => "penny",
+                     "count" => 0,
+                     "interval" => 1}}
+
+    until remainder.==(0)
+      denom.each do |key, coin|
+        until remainder.<(coin.fetch('interval'))
+          count = coin.fetch('count')
+
+          if count.>=(1)
+            coin.store('denom', coin.fetch('denom').concat('s'))
+            coin.store('denom', (coin.fetch('denom')).sub('y','ie'))
+          end
+
+          coin.store('count', count.+(1))
+          remainder = remainder.-(coin.fetch('interval'))
+        end
+      end
+      remainder = 0
     end
+
+    result = []
+    denom.each do |key, coin|
+      count = coin.fetch('count')
+      if count.>(0)
+        result.push("#{count} #{coin.fetch('denom')}")
+      end
+    end
+
+    final = result.join(", ")
+    final = final.reverse()
+    final = final.sub(',','dna ')
+    final = final.reverse()
   end
 end
--- spec/your_change_spec.rb
+++ spec/your_change_spec.rb
@@ -2,7 +2,22 @@ require('rspec')
 require('your_change')
 
 describe('Fixnum#your_change') do
-  it('accepts 4 cents from user and returns 4 pennies') do
-     expect(4.your_change()).to(eq('4 pennies')) 
+  it('accepts an amount and returns how many quarters') do
+    expect(50.your_change()).to(eq('2 quarters'))
+    expect(25.your_change()).to(eq('1 quarter'))
+  end
+
+  it('accepts an amount and returns how many dimes') do
+    expect(70.your_change()).to(eq('2 quarters and 2 dimes'))
+    expect(60.your_change()).to(eq('2 quarters and 1 dime'))
+  end
+
+  it('accepts an amount and returns how many nickels') do
+    expect(65.your_change()).to(eq('2 quarters, 1 dime and 1 nickel'))
+  end
+
+  it('accepts and amount and returns how many pennies') do
+    expect(67.your_change()).to(eq('2 quarters, 1 dime, 1 nickel and 2 pennies'))
+    expect(1.your_change()).to(eq('1 penny'))
   end
 end
